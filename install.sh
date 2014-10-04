c847e043e88ab5beccbd156d4a7bfcb3fa899614
T Kwon <tdkwon@gmail.com>
2014-10-03 21:38:41 -0700

    fixed some typos

--- install.sh
+++ install.sh
@@ -11,7 +11,6 @@ ln -s /usr/lib/`uname -i`-linux-gnu/libjpeg.so /usr/lib/
 ln -s /usr/lib/`uname -i`-linux-gnu/libz.so /usr/lib/
 ln -s /usr/include/freetype2 /usr/local/include/freetype
 pip install virtualenv virtualenvwrapper
-cat "source /usr/local/bin/virtualenvwrapper.sh >> .bashrc"
-cat "PATH=$PATH:." >> .bashrc
-source .bashrc
-mkvirtualenv dev
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> /home/vagrant/.bashrc
+echo "PATH=\$PATH:." >> /home/vagrant/.bashrc
+
